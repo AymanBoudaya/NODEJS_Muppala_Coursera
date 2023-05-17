d12a49ec67dd27db687db1dff0e3f12b0ec29d11
AymanBoudaya <ayman.boudaya@yahoo.fr>
2023-05-17 22:05:28 +0100

    File Upload

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var path = require('path');
 var logger = require('morgan');
 var passport = require('passport')
 var config = require('./config');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter')
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -44,6 +46,12 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 
+app.use(session({
+  secret: '123456789',
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -52,6 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
