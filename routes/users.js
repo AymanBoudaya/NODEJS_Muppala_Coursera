f08329c069bb5c0672929df8eea825081f50a2c5
AymanBoudaya <ayman.boudaya@yahoo.fr>
2023-05-18 23:43:57 +0100

    Cors

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var passport = require('passport');
 var User = require('../models/user');
 var authenticate = require('../authenticate');
+const cors = require('./cors');
 
 const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/',authenticate.verifyUser,authenticate.verifyAdmin, function (req, res, next) {
+router.get('/',cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin, function (req, res, next) {
   User.find({}) 
     .then((users) => {
         res.statusCode = 200;
@@ -18,7 +19,7 @@ router.get('/',authenticate.verifyUser,authenticate.verifyAdmin, function (req, 
     .catch((err) => next(err));
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({ username: req.body.username }),
     req.body.password, (err, user) => {
       if (err) {
@@ -47,7 +48,7 @@ router.post('/signup', (req, res, next) => {
     })
 });
 
-router.post('/login', passport.authenticate('local'), (req, 
+router.post('/login',cors.corsWithOptions, passport.authenticate('local'), (req, 
   res) => { //when
   
     var token = authenticate.getToken({ _id: req.user._id,admin:req.user.admin });
